6f6bcfe84afb38db45c106336d61ecf510b219d7
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-05-24 12:58:09 -0400

    simplify example

--- examples/basic.py
+++ examples/basic.py
@@ -1,12 +1,14 @@
 # isort: skip_file
 # idk why but htis order of imports matters
 from mda_simulator import ImageGenerator
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 
 # without hte above order hyperslicer doesn't work
 
 import numpy as np
-from mpl_interactions import hyperslicer
+
+# from mpl_interactions import hyperslicer
 
 
 img_gen = ImageGenerator(10000)
@@ -29,8 +31,9 @@ for t in range(T):
     for p in range(P):
         for zi, z in enumerate(np.linspace(-30, 30, Z)):
             out[t, p, zi] = img_gen.snap_img(image_locations[p], z=z)
-            out_rgb[t, p, zi] = img_gen.snap_img(image_locations[p], z=z, as_rgb=True)
+            # out_rgb[t, p, zi] = img_gen.snap_img(image_locations[p], z=z, as_rgb=True)
     img_gen.step_positions()
-plt.figure()
-hyperslicer(out_rgb, is_color_image=True)
-plt.show()
+# plt.figure()
+# hyperslicer(out_rgb, is_color_image=True)
+# hyperslicer(out_rgb, is_color_image=True)
+# plt.show()
